8456d262f2007d51f24996209802487918f33f9b
Nathanael Grier <Nathanael.Grier@Smoothstack.com>
2021-07-29 18:45:48 -0500

    correct file structure

--- src/main/java/com/beardtrust/webapp/accountservice/controllers/AccountController.java
+++ src/main/java/com/beardtrust/webapp/accountservice/controllers/AccountController.java
@@ -1,32 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package com.beardtrust.webapp.accountservice.controllers;
 
 import com.beardtrust.webapp.accountservice.entities.AccountEntity;
+import com.beardtrust.webapp.accountservice.repos.AccountRepository;
 import com.beardtrust.webapp.accountservice.services.AccountService;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ *
+ * @author Nathanael <Nathanael.Grier at your.org>
+ */
 @RestController
 @RequestMapping(path = "/accounts")
-@Slf4j
 public class AccountController {
     
     @Autowired
-    private AccountService as;
-
-	@GetMapping
-	private String test(){
-		return "Working";
-	}
-        
-        @PostMapping
-        @PreAuthorize(value = "hasAuthority('admin')")
-        private AccountEntity createAccount(@RequestBody AccountEntity a) {
-            return as.createService(a);
-        }
+    private AccountRepository repo;
+    
+    @Autowired
+    private AccountService as = new AccountService(repo);
+    
+    @PostMapping
+    public AccountEntity createAccount(@RequestBody AccountEntity a) {
+        return as.createService(a);
+    }
+    
 }
---
+++ src/test/java/com/beardtrust/webapp/accounts/AccountsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.beardtrust.webapp.accounts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
